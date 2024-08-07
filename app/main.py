ff5cdfe9584ceca058e15570ca0f06627fd968f5
Anastasiia Karpenko <karpenko.anastasiya@gmail.com>
2024-08-07 14:57:10 +0300

    add some magic methods

--- app/main.py
+++ app/main.py
@@ -1,3 +1,27 @@
+from __future__ import annotations
+
+
 class Vector:
-    # write your code here
-    pass
+    def __init__(self, x: int | float, y: int | float) -> None:
+        self.x = round(x, 2)
+        self.y = round(y, 2)
+
+    def __add__(self, other: int | float) -> Vector:
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __sub__(self, other: int | float) -> Vector:
+        x = self.x - other.x
+        y = self.y - other.y
+        return Vector(x, y)
+
+    def __mul__(self, other: int | float | Vector) -> int | float | Vector:
+        if isinstance(other, Vector):
+            return self.x * other.x + self.y * other.y
+        if isinstance(other, (int, float)):
+            x = self.x * other
+            y = self.y * other
+            return Vector(x, y)
+
+
